d56e92d3ed39ce1865e3c1a61f84de6e3c166964
Victor <hoodrich10135@gmail.com>
2025-10-21 16:47:18 -0700

    Add project files.

---
+++ HoodsEnergyDrinks-CSharp/HoodsEnergyDrinks.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System;
+using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+using SPTarkov.Server.Core.Services;
+using SPTarkov.Server.Core.Utils;
+using SPTarkov.Server.Core.Utils.Json;
+
+namespace HoodsEnergyDrinks_CSharp;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string Name { get; init; } = "HoodsEnergyDrinks";
+    public override string Author { get; init; } = "SPTarkov";
+    public override List<string>? Contributors { get; init; }
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.7");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.1");
+
+
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string? License { get; init; } = "MIT";
+    public override string ModGuid { get; init; } = "hood.moreenergydrinks";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostSptModLoader + 1)]
+public class HoodsEnergyDrinks(
+    ISptLogger<HoodsEnergyDrinks> logger,
+    ConfigServer configServer,
+    ModHelper modHelper,
+    DatabaseService databaseService,
+    JsonUtil jsonUtil,
+    FluentTraderAssortCreator fluentAssortCreator
+    ) 
+    : IOnLoad
+{
+    public Task OnLoad() 
+    {
+        logger.Success("EnergyDrinksStart...");
+        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        var configPath = Path.GetFullPath(Path.Combine(pathToMod, "config"));
+        var config = modHelper.GetJsonDataFromFile<ModConfig>(configPath, "config.jsonc");
+        //logger.Success($"Here is result: {config.alternate_flea_price}"); // test use case
+
+
+
+
+
+
+
+        //logger.Success(test.config);
+        logger.Success("More energy drinks have been added to the server!");
+        return Task.CompletedTask;
+    }
+}
